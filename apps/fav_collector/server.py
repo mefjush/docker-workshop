73c74ca7fa4b5c5796125785f156e029f7c87d6b
Mateusz Michalowski <mateusz@michalowski.biz>
2015-07-30 23:54:18 +0200

    cleanup

--- apps/fav_collector/server.py
+++ apps/fav_collector/server.py
@@ -5,9 +5,8 @@ from os.path import isfile, join
 import urllib
 import xml.etree.ElementTree as ET
 
-dir = os.path.dirname(os.path.realpath(__file__))
-static_dir = dir + "/static"
-
+rates_url = os.environ.get('RATES_URL', "http://localhost:8888")
+static_dir = join(os.path.dirname(os.path.realpath(__file__)), "static")
 render = web.template.render('templates/')
 
 urls = (
@@ -18,38 +17,29 @@ urls = (
 
 class index:        
     def GET(self):
-        onlyfiles = [ f for f in listdir(static_dir) if isfile(join(static_dir,f)) ]
-        return render.index(onlyfiles)
+        files = [ f for f in listdir(static_dir) if isfile(join(static_dir, f)) ]
+        return render.index(files)
 
 class submit:
     def GET(self):
-        data = web.input(url="")
-        url = data.url
-        if ("http" not in url):
-            url = "http://" + url
-
-        if (not(url.endswith("/"))):
-            url = url + "/"
-        
-        filename = url.replace("http://", "").replace("https://", "").replace("www.", "").replace("/", "") + ".ico"
-
-        urllib.urlretrieve(url + "favicon.ico", static_dir + "/" + filename)
-
+        url = web.input(url="").url
+        url = url if ("http" in url) else ("http://" + url) 
+        url = url if (url.endswith("/")) else (url + "/")
+       
+        filename = reduce(lambda url, s : url.replace(s, ""), ["http://", "https://", "www.", "/"], url)
+        urllib.urlretrieve(url + "favicon.ico", join(static_dir, filename))
         raise web.redirect("/")
 
 class currency:
     def GET(self, curr):
-        curr = curr.upper()
-        req = urllib.urlopen("http://localhost:8888")
-        root = ET.fromstring(req.read())
-        query = ".//*[@currency='" + curr + "']"
+        currency = curr.upper()
+        req = urllib.urlopen(rates_url)
+        xmlRoot = ET.fromstring(req.read())
+        query = ".//*[@currency='" + currency + "']"
+        nodes = xmlRoot.findall(query)
 
-        nodes = root.findall(query)
-
-        if (len(nodes) > 0):
-            return "EUR/" + curr + " = " + nodes[0].get('rate')
-        else:
-            return "Error"        
+        value = nodes[0].get('rate') if (len(nodes) > 0) else "unknown"
+        return "EUR/" + currency + " = " + value 
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
