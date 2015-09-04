72da679d67f7f84608527c0eec3085342e682dd8
Mateusz Michalowski <mateusz@adhese.eu>
2015-09-04 14:44:04 +0200

    updates

--- workshop/05_links/server.sh
+++ workshop/05_links/server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 URL="http://$RATES_PORT_8080_TCP_ADDR:$RATES_PORT_8080_TCP_PORT"
 echo "Rates service url: ${URL}"
-export RATES_URL="${URL}";
+export RATES_URL="${URL}"
 
 python -u server.py 8888 
 
