c5daca839cc3fe1f8083558c881949410b06d129
Mateusz Michalowski <mateusz@michalowski.biz>
2015-09-05 23:13:56 +0200

    add logging to rates

--- workshop/02_rates/rates/server.py
+++ workshop/02_rates/rates/server.py
@@ -1,6 +1,21 @@
 import web
 import urllib
+import logging
+import time
+import os
 import xml.etree.ElementTree as ET
+
+if not os.path.exists('logs'):
+    os.makedirs('logs')
+
+logging.basicConfig(level=logging.INFO)
+
+handler = logging.FileHandler('logs/rates.log')
+handler.setLevel(logging.INFO)
+handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+
+logger = logging.getLogger('rates')
+logger.addHandler(handler)
 
 urls = (
     '/rates/eur/(.*)', 'rates'
@@ -8,12 +23,19 @@ urls = (
 
 class rates:
     def GET(self, currency):
+        start = time.time()
+
+        currency = currency.upper()
         req = urllib.urlopen("http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml")
         xmlRoot = ET.fromstring(req.read())
-        query = ".//*[@currency='" + currency.upper() + "']"
+        query = ".//*[@currency='" + currency + "']"
         nodes = xmlRoot.findall(query)
 
         rate = nodes[0].get('rate') if (len(nodes) > 0) else 0
+
+        millis = (time.time() - start) * 1000
+        logger.info('currency: ' +  currency + ' - rate: ' + str(rate)  + ' - millis: ' + str(millis))
+
         return rate 
 
 
