d435c8a5f9ba00cc6a7f168f30c3c8c933465433
Mateusz Michalowski <mateusz@michalowski.biz>
2015-09-09 20:19:47 +0200

    startup notes

--- slides/00_prepare_shell.sh
+++ slides/00_prepare_shell.sh
@@ -1,3 +1,13 @@
-#source ...
+# source ...
+# grunt serve
+# cd workshop/02_rates/rates
+# python server.py
+
+# cd workshop/03_portal/portal
+# python server.py 8888
+
+# http://0.0.0.0:8000/
+# http://localhost:8888/
+# http://localhost:8080/rates/eur/usd
 export PS1='\W$ ' 
 export PATH=$PATH:/home/mefju/mefju/docker-workshop/slides/bin
